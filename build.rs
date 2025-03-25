6b0dd62c95ded7acade938b4f7e483547cfb41b1
mrdkprj <rockfield.d.m@gmail.com>
2025-03-25 16:21:19 +0900

    WIN_HOOK_DLL

--- build.rs
+++ build.rs
@@ -5,10 +5,10 @@ use std::path::PathBuf;
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let dll_src = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "\\win_hook.dll"));
-    let dll_dest = out_dir.join("project_b.dll");
+    let dll_dest = out_dir.join("win_hook.dll");
 
     let msg = format!("Failed to copy DLL:{}", out_dir.to_str().unwrap());
     fs::copy(&dll_src, &dll_dest).unwrap_or_else(|_| panic!("{}", msg));
 
-    println!("cargo:rustc-env=PROJECT_B_DLL={}", dll_dest.display());
+    println!("cargo:rustc-env=WIN_HOOK_DLL={}", dll_dest.display());
 }
