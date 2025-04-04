eb554727a3eaa3df47947ba41da13553b43afcb7
mrdkprj <rockfield.d.m@gmail.com>
2025-04-04 19:39:33 +0900

    webview only for windows

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::{env, path::PathBuf};
 fn main() {
     println!("cargo::rerun-if-changed=build.rs");
 
-    if cfg!(feature = "webview") {
+    if cfg!(feature = "webview") && cfg!(windows) {
         let dll_src = PathBuf::from(env::var("DEP_WCPOPUPHOOK_DLL").unwrap());
         println!("cargo:rustc-env=WIN_HOOK_DLL={}", dll_src.display());
     }
